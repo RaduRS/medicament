039397341e5410c06feaa6c812ab26da2c77c8ed
RaduRS <rsrusu90@gmail.com>
2023-03-02 09:40:37 +0000

    update server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
 const PORT = process.env.PORT || 8000;
 const pillRoutes = require("./routes/pillRoute");
@@ -17,18 +16,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api", pillRoutes);
 
-mongoose
-  .set("strictQuery", false)
-  .connect(process.env.MONGO_URI)
-  .then(() => {
     app.listen(PORT, () => {
       console.log(`is running on port ${PORT}`);
-      //   setInterval(() => {
-      //     console.log("Pinging the app to keep it awake...");
-      //     fetch("https://<your-project-name>.glitch.me")
-      //       .then((response) => console.log("App is awake"))
-      //       .catch((error) => console.log("Error while pinging the app"));
-      //   }, 4 * 60 * 1000); // ping every 4 minutes
+        setInterval(() => {
+          console.log("Pinging the app to keep it awake...");
+          fetch("https://medicament.onrender.com")
+            .then((response) => console.log("App is awake"))
+            .catch((error) => console.log("Error while pinging the app"));
+        }, 10 * 60 * 1000); // ping every 10 minutes
     });
-  })
-  .catch((err) => console.log(err));
+
+
